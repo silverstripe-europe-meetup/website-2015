7a41999b947c371040b990a5dcec1e3685986439
Simon 'Sphere' Erkelens <simon@casa-laguna.net>
2015-09-10 19:24:51 +0200

    ctrl-alt-L

--- mysite/code/Extensions/ExtendedIterator.php
+++ mysite/code/Extensions/ExtendedIterator.php
@@ -1,21 +1,28 @@
 <?php
 
-
-class ExtendedIterator implements TemplateIteratorProvider {
+/**
+ * Class ExtendedIterator does things with the template iterator
+ * @inheritdoc
+ */
+class ExtendedIterator implements TemplateIteratorProvider
+{
 
 	protected $iteratorPos;
 	protected $iteratorTotalItems;
 
-	public static function get_template_iterator_variables() {
+	public static function get_template_iterator_variables()
+	{
 		return array('PosGold');
 	}
 
-	public function iteratorProperties($pos, $totalItems) {
-		$this->iteratorPos        = $pos;
+	public function iteratorProperties($pos, $totalItems)
+	{
+		$this->iteratorPos = $pos;
 		$this->iteratorTotalItems = $totalItems;
 	}
 
-	public function PosGold() {
+	public function PosGold()
+	{
 		$platinum = Sponsor::get()->filter(array('Type' => 'Platinum'))->count();
 		return ($this->iteratorPos + $platinum) % 2;
 	}
