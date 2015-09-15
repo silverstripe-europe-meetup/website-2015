721c764b688d1837395576479cd3a7827d2b0399
Simon 'Sphere' Erkelens <simon@casa-laguna.net>
2015-09-16 01:10:32 +0200

    Revert "Better SCSS clearing and such."
    This reverts commit 09916cbf3e0cbf4ac0c86ad07d63d0ac1f840675.

--- mysite/code/Pages/Page.php
+++ mysite/code/Pages/Page.php
@@ -119,12 +119,14 @@ class Page_Controller extends \ContentController
 			project() . '/scss/sponsors.scss',
 			project() . '/scss/talk.scss',
 			project() . '/scss/legacy.scss',
-			project() . '/scss/editor.scss'
 		]);
+		Requirements::css(project() . '/scss/editor.scss');
 		Requirements::clear(project() . '/css/editor.css');
 	}
 
 	public function onAfterWrite(){
 		parent::onAfterWrite();
+
+
 	}
 }
