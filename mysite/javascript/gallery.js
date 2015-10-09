4b7b2af55767cde3a3988f1a846801409b4f1f6c
Simon 'Sphere' Erkelens <simon@casa-laguna.net>
2015-10-09 20:48:41 +0200

    Gallery updates

--- mysite/javascript/gallery.js
+++ mysite/javascript/gallery.js
@@ -2,13 +2,13 @@
 	$('a.gallery-item__specific').on('click', function () {
 		var hash = $(this).data('gallery');
 		var galleryItem = $('#gallery-item');
-		galleryItem.hide(500).html('');
+		galleryItem.hide(100).html('');
 		$.ajax({
 			type: 'POST',
 			url: '/home/getGallery',
 			data: {'gallery': hash},
 			success: function (data) {
-				galleryItem.html(data).show(500);
+				galleryItem.html(data).show(100);
 				galleryItem.animate({
 					scrollTop: galleryItem.top - 70
 				}, 700);
