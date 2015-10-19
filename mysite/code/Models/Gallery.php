40d0e334f965a5e5d11a94f5878751fb8c3b28c8
Simon 'Sphere' Erkelens <simon@casa-laguna.net>
2015-10-19 15:35:02 +0200

    Not editable anymore :'(

--- mysite/code/Models/Gallery.php
+++ mysite/code/Models/Gallery.php
@@ -45,7 +45,6 @@ class Gallery extends DataObject
 		$gridFieldConfig->addComponent(new GridFieldBulkUpload());
 		$gridFieldConfig->addComponent(new GridFieldSortableRows('SortOrder'));
 		$gridFieldConfig->addComponent(new GridFieldGalleryTheme('CMSThumbnail'));
-		$gridFieldConfig->addComponent(new GridFieldEditableColumns());
 		$gridFieldConfig->removeComponentsByType('GridFieldPaginator');
 		$gridFieldConfig->removeComponentsByType('GridFieldPageCount');
 		$field = GridField::create(
